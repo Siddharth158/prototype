a0073a98da6b3a56a0a4f284153b4030c003a46f
siddharth prabhu <siddharthprabhuu@gmail.com>
2023-06-27 16:37:25 +0530

    added all resorces

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'sky-green': 'rgba(67, 223, 155, 1)',
+        'color1': 'rgba(61, 186, 181, 1)',
+        'color2': 'rgba(61, 186, 181, 0)',
+      },
+    },
+  },
+  plugins: [],
+}
+
