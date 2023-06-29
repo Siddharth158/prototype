c6dc45a01d26c5c50ccefe7d1bd3e1105726605a
siddharth prabhu <siddharthprabhuu@gmail.com>
2023-06-29 23:17:11 +0530

    have doone some css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@
 module.exports = {
   content: ["*"],
   theme: {
+    
     extend: {
       colors: {
         'sky-green': 'rgba(67, 223, 155, 1)',
         'color1': 'rgba(61, 186, 181, 1)',
         'color2': 'rgba(61, 186, 181, 0)',
+      },
+      fontFamily: {
+        'proxima': ['Proxima Nova', 'sans-serif'],
       },
     },
   },
